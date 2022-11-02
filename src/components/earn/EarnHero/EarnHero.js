e39ebe395a1e963598a65480f19461d25ac05383
LorenaL579 <lorena.lici579@gmail.com>
2022-11-02 01:02:01 +0100

    update for dashboard

--- src/components/earn/EarnHero/EarnHero.js
+++ src/components/earn/EarnHero/EarnHero.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import earnHeroStats from '../../../assets/earnAssets/earnHeroStats.webp'
-import coin from '../../../assets/earnAssets/coin.webp'
-import coinBackground from '../../../assets/earnAssets/coinBackground.webp'
-import eth from '../../../assets/earnAssets/ethereumIcon.svg'
-import bnb from '../../../assets/earnAssets/bnbIcon.svg'
-import avax from '../../../assets/earnAssets/avaxIcon.svg'
-import getFormattedNumber from '../../../functions/get-formatted-number'
-import CountUp from 'react-countup'
-import axios from 'axios'
-
+import React, { useEffect, useState } from "react";
+import earnHeroStats from "../../../assets/earnAssets/earnHeroStats.webp";
+import coin from "../../../assets/earnAssets/coin.webp";
+import coinBackground from "../../../assets/earnAssets/coinBackground.webp";
+import eth from "../../../assets/earnAssets/ethereumIcon.svg";
+import bnb from "../../../assets/earnAssets/bnbIcon.svg";
+import avax from "../../../assets/earnAssets/avaxIcon.svg";
+import getFormattedNumber from "../../../functions/get-formatted-number";
+import CountUp from "react-countup";
+import axios from "axios";
 
 const EarnHero = () => {
-
   const [totalpaid, setTotalPaid] = useState();
 
   const getTotalPaidData = async () => {
@@ -24,59 +22,70 @@ const EarnHero = () => {
     getTotalPaidData();
   }, []);
 
-
   return (
     <div className="row w-100 earn-hero p-4 justify-content-between">
-        <div className="col-5 d-flex flex-column justify-content-center gap-3">
-          <h3 className='text-white'>DYP Earn</h3>
-          <p className="text-white">Make the most of your assets with Dypius Earn products. Start earning today!</p>
-        </div>
-        <div className="col-6 d-flex gap-5">
-          <div className="d-flex flex-column align-items-start">
-         <div className="d-flex flex-column" style={{marginLeft: '80px'}}>
-         <p style={{fontSize: '9px', color: '#F36D46', fontWeight: '300'}}>Rewards paid out</p>
+      <div className="col-5 d-flex flex-column justify-content-center gap-3">
+        <h3 className="text-white">DYP Earn</h3>
+        <p className="text-white">
+          Make the most of your assets with Dypius Earn products. Start earning
+          today!
+        </p>
+      </div>
+      <div className="col-6 d-flex gap-5">
+        <div className="d-flex flex-column align-items-start">
+          <div className="d-flex flex-column" style={{ marginLeft: "80px" }}>
+            <p style={{ fontSize: "9px", color: "#F36D46", fontWeight: "300" }}>
+              Rewards paid out
+            </p>
             <CountUp
-                      style={{fontSize: '19px', color: '#F36D46', fontWeight: '600', textAlign: 'start'}}
-                      start={totalpaid?.totalPaidInUsd - 400.0}
-                      end={totalpaid?.totalPaidInUsd}
-                      duration={120}
-                      separator=","
-                      decimals={2}
-                      prefix="$"
-                    />
-         </div>
-            <img src={earnHeroStats} style={{width: '230px', height: '80px' }} alt="" />
+              style={{
+                fontSize: "19px",
+                color: "#F36D46",
+                fontWeight: "600",
+                textAlign: "start",
+              }}
+              start={totalpaid?.totalPaidInUsd - 400.0}
+              end={totalpaid?.totalPaidInUsd}
+              duration={120}
+              separator=","
+              decimals={2}
+              prefix="$"
+            />
           </div>
-          <div className="d-flex flex-column justify-content-between">
-            <div className="d-flex justify-content-start align-items-center gap-2">
-              <img src={eth} alt="" />
-              <h4 style={{color: '#6C85E7'}}> {getFormattedNumber(
-                      totalpaid?.ethTotal.wethPaiOutTotals,
-                      0
-                    )}</h4>
-            </div>
-            <div className="d-flex justify-content-start align-items-center gap-2">
-              <img src={bnb} alt="" />
-              <h4 style={{color: '#F6CC29'}}>{getFormattedNumber(
-                      totalpaid?.bnbTotal.wbnbPaidOutTotals,
-                      0
-                    )}</h4>
-            </div>
-            <div className="d-flex justify-content-start align-items-center gap-2">
-              <img src={avax} alt="" />
-              <h4 style={{color: '#F86667'}}>{getFormattedNumber(
-                      totalpaid?.avaxTotal.avaxPaidOutTotals,
-                      0
-                    )}</h4>
-            </div>
+          <img
+            src={earnHeroStats}
+            style={{ width: "230px", height: "80px" }}
+            alt=""
+          />
+        </div>
+        <div className="d-flex flex-column justify-content-between">
+          <div className="d-flex justify-content-start align-items-center gap-2">
+            <img src={eth} alt="" />
+            <h4 style={{ color: "#6C85E7" }}>
+              {" "}
+              {getFormattedNumber(totalpaid?.ethTotal.wethPaiOutTotals, 0)}
+            </h4>
           </div>
-          <div className="position-relative">
-            <img src={coin} alt="" className="coin" />
-            <img src={coinBackground} alt="" className="coin" />
+          <div className="d-flex justify-content-start align-items-center gap-2">
+            <img src={bnb} alt="" />
+            <h4 style={{ color: "#F6CC29" }}>
+              {getFormattedNumber(totalpaid?.bnbTotal.wbnbPaidOutTotals, 0)}
+            </h4>
+          </div>
+          <div className="d-flex justify-content-start align-items-center gap-2">
+            <img src={avax} alt="" />
+            <h4 style={{ color: "#F86667" }}>
+              {getFormattedNumber(totalpaid?.avaxTotal.avaxPaidOutTotals, 0)}
+            </h4>
           </div>
         </div>
+        <div className="position-relative">
+          <img src={coin} alt="" className="coin" />
+          <img src={coinBackground} alt="" className="coin" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EarnHero
+export default EarnHero;
--- src/components/newsCard/NewsCard.js
+++ src/components/newsCard/NewsCard.js
@@ -1,10 +1,15 @@
 import React from "react";
+import filledArrow from './assets/filledArrow.svg'
 import './newscard.css'
 
-const NewsCard = () => {
+const NewsCard = ({image, title}) => {
   return (
     <div className="newscard-wrapper">
-      <div>NewsCard</div>
+      <div className="d-flex flex-column gap-2 position-relative">
+        <img src={require(`./assets/${image}`).default} alt='' className="newsimg"/>
+        <h6 className="nc-title">{title}</h6>
+        <img src={filledArrow} alt='' className="ncarrow"/>
+      </div>
     </div>
   );
 };
