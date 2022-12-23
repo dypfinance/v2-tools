a88aaae48f9e33878b46b4b495ab5078a54c8e8f
AldiAlinj <aldialinj0@gmail.com>
2022-12-23 11:10:53 +0100

    fixed swap

--- src/App.js
+++ src/App.js
@@ -565,7 +565,7 @@ class App extends React.Component {
                   <Route
                     exact
                     path="/swap"
-                    render={() => <Swap/>}
+                    component={Swap}
                   />
 
                   {/* <Route
