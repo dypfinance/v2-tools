6ddcf5a47e61de0874b9aa2251e23dd4b81012cb
doyledyp <doyledev@protonmail.com>
2022-12-27 18:35:18 +0200

    Revert "logout when click on swap"
    This reverts commit 542c9e1fb4d0a945be34fbc052da9d0f2440aa66.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -19,9 +19,9 @@ import { useEagerConnect, useInactiveListener } from "../../functions/hooks";
 import toolsLogo from "../../assets/sidebarIcons/toolsLogo.svg";
 import toolsLogoActive from "../../assets/sidebarIcons/toolsLogoActive.svg";
 import accordionIndicator from "../../assets/sidebarIcons/accordionIndicator.svg";
-import sidebarDypius from "../../assets/sidebarDypius.svg";
-import "./sidebar.css";
-import navRadius from "../../assets/navRadius.svg";
+import sidebarDypius from '../../assets/sidebarDypius.svg'
+import './sidebar.css'
+import navRadius from '../../assets/navRadius.svg'
 import useWindowSize from "../../functions/useWindowSize";
 
 const activateLasers = () => {
@@ -68,291 +68,267 @@ const Sidebar = (props) => {
     }
   }, [account]);
 
-  const windowSize = useWindowSize();
+  const windowSize = useWindowSize()
 
   const openSidebar = () => {
-    if (windowSize.width < 1800) {
+    if(windowSize.width < 1800){
       setActiveSidebar(true);
-    } else if (windowSize.width > 1800) {
-      setActiveSidebar(true);
+    }else if(windowSize.width > 1800){
+      setActiveSidebar(true)
     }
   };
 
   const closeSidebar = () => {
-    if (windowSize.width < 1800) {
+    if(windowSize.width < 1800){
       setActiveSidebar(false);
-    } else if (windowSize.width > 1800) {
-      setActiveSidebar(true);
+    }else if(windowSize.width > 1800){
+      setActiveSidebar(true)
     }
   };
+  
+  const sidebar = document.querySelector('.testbar')
 
-  const sidebar = document.querySelector(".testbar");
-
-  sidebar?.addEventListener("mouseover", openSidebar);
-  sidebar?.addEventListener("mouseleave", closeSidebar);
+  sidebar?.addEventListener("mouseover", openSidebar)
+  sidebar?.addEventListener("mouseleave", closeSidebar)
 
   useEffect(() => {
     // const fetchInterval = setInterval(
     //   () => setlocation(window.location.pathname),
     //   1000
     // );
-    if (windowSize.width > 1800) {
-      setActiveSidebar(true);
-    } else if (windowSize.width < 1800) {
-      setActiveSidebar(false);
+    if(windowSize.width > 1800){
+      setActiveSidebar(true)
+    }else if(windowSize.width < 1800){
+      setActiveSidebar(false)
     }
+    
   }, [windowSize]);
+
 
   const sidebarItems = [
     {
       label: "Earn",
       icon: "earnIcon",
-      link: "/earn",
+      link: '/earn'
     },
     {
       label: "Governance",
       icon: "governanceIcon",
-      link: "/governance",
+      link: '/governance'
     },
     {
       label: "Bridge",
       icon: "bridgeIcon",
-      link: "/bridge",
+      link: '/bridge'
     },
     {
       label: "Explorer",
       icon: "explorerIcon",
       children: [
         {
-          title: "Pair explorer",
-          link: "/pair-explorer",
+          title: 'Pair explorer',
+          link: '/pair-explorer'
         },
         {
-          title: "Pool explorer",
-          link: "/pool-explorer",
+          title: 'Pool explorer',
+          link: '/pool-explorer'
         },
         {
-          title: "Big Swap",
-          link: "/big-swap-explorer",
+          title: 'Big Swap',
+          link: '/big-swap-explorer'
         },
         {
-          title: "Top Tokens",
-          link: "/top-tokens",
+          title: 'Top Tokens',
+          link: '/top-tokens'
         },
         {
-          title: "Yields",
-          link: "/farms",
+          title: 'Yields',
+          link: '/farms'
         },
         {
-          title: "Submit Form",
-          link: "/submit-info",
+          title: 'Submit Form',
+          link: '/submit-info'
         },
+       
       ],
     },
     {
       label: "Projects",
       icon: "projectsIcon",
-      children: [
-        {
-          title: "Launchpad",
-          link: "/launchpad",
-        },
-        {
-          title: "DYP Locker",
-          link: "/locker",
-        },
-      ],
+      children: [  {
+        title: 'Launchpad',
+        link: '/launchpad'
+      },
+      {
+        title: 'DYP Locker',
+        link: '/locker'
+      },
+    ],
     },
     {
       label: "Swap",
       icon: "swapIcon",
-      link: "/swap",
+      link: "/swap"
     },
     {
       label: "News",
       icon: "newsIcon",
-      link: "/news",
+      link: '/news'
     },
   ];
+
+  
 
   const sidebarItem = document.querySelectorAll(".sidebar-item");
 
   const windowUrl = window.location.href;
 
+
+
   return (
+
+
     <div
       id="sidebar"
-      style={{ padding: "2.5rem 0" }}
+      style={{padding: '2.5rem 0'}}
       className={`testbar ${
         activeSidebar ? "testbar-open" : null
       } d-none d-lg-flex flex-column justify-content-between align-items-start`}
     >
-      <img
-        src={navRadius}
-        className={`nav-radius ${activeSidebar && "nav-radius-open"}`}
-        alt=""
-      />
-      <div className="w-100">
-        <div className="d-flex w-100 justify-content-center align-items-center pb-5">
-          <NavLink to="/" onClick={() => setActiveLink("")}>
-            <img
-              src={activeSidebar ? toolsLogoActive : toolsLogo}
-              alt=""
-              style={{ height: "40px" }}
-            />
-          </NavLink>
-        </div>
-        <div
-          className={`sidebar-container w-100 justify-content-center ${
-            activeSidebar ? "align-items-start" : "align-items-center"
-          } d-flex flex-column gap-4`}
-          style={{ gap: 35 }}
+      <img src={navRadius} className={`nav-radius ${activeSidebar && 'nav-radius-open'}`} alt="" />
+     <div className="w-100">
+     <div className="d-flex w-100 justify-content-center align-items-center pb-5">
+        <NavLink to='/'
+        onClick={() => setActiveLink("")}
         >
-          <div
-            className={`sidebar-container w-100 accordion justify-content-center ${
-              activeSidebar ? "align-items-start" : "align-items-center"
-            } d-flex flex-column gap-4`}
-            id="accordionExample2"
-          >
-            {sidebarItems.map((sideItem, index) =>
-              sideItem.children?.length > 0 ? (
-                <>
-                  <div
-                    data-bs-toggle="collapse"
-                    data-bs-target={`#collapse${sideItem.label}`}
-                    aria-expanded="true"
-                    aria-controls={`collapse${sideItem.label}`}
-                    id={`heading${sideItem.label}`}
-                    className={`sidebar-item gap-3 p-2 d-flex ${
-                      activeSidebar
-                        ? "active-width justify-content-start ms-4"
-                        : "justify-content-center"
-                    } align-items-center ${
-                      activeLink === sideItem.label ? "active-side-link" : null
-                    }`}
-                    onClick={() => setActiveLink(sideItem.label)}
-                    onMouseEnter={() => setHover(sideItem.label)}
-                    onMouseLeave={() => setHover(null)}
-                  >
-                    <img
-                      src={
-                        require(`../../assets/sidebarIcons/${
-                          activeLink === sideItem.label ||
-                          hover === sideItem.label
-                            ? sideItem.icon + "Active.svg"
-                            : sideItem.icon + ".svg"
-                        }`).default
-                      }
-                      alt=""
-                      style={{ width: 32, height: 32 }}
-                    />
-                    {activeSidebar && (
-                      <div className="d-flex w-100 flex-row align-items-center justify-content-between">
-                        <h3
-                          className={
-                            activeLink === sideItem.label ||
-                            hover === sideItem.label
-                              ? "active-text"
-                              : "sideitem-text"
-                          }
-                        >
-                          {sideItem.label}
-                        </h3>
-                        <img
-                          src={accordionIndicator}
-                          alt="indicator"
-                          id="indicator"
-                        />
-                      </div>
-                    )}
-                  </div>
-                  <div
-                    id={`collapse${sideItem.label}`}
-                    className={`accordion-collapse collapse ${
-                      sideItem.children.filter((obj) => {
-                        return windowUrl.includes(obj.link);
-                      })
-                        ? "open"
-                        : null
-                    }`}
-                    aria-labelledby={`heading${sideItem.label}`}
-                    data-bs-parent="#accordionExample2"
-                  >
-                    {activeSidebar ? (
-                      <div className="accordion-container d-flex flex-column gap-2 ms-5">
-                        {sideItem.children.map((child, index) => (
-                          <NavLink
-                            key={index}
-                            to={child.link}
-                            className={(isActive) =>
-                              isActive
-                                ? "accordion-child accordion-child-active"
-                                : "accordion-child"
-                            }
-                          >
-                            {child.title}
-                          </NavLink>
-                        ))}
-                      </div>
-                    ) : null}
-                  </div>
-                </>
-              ) : (
-                sideItem.link?.length > 0 && (
-                  <NavLink to={sideItem.link}>
-                    <div
-                      key={index}
-                      id={sideItem.label}
-                      className={`sidebar-item gap-3 p-2 d-flex ${
-                        activeSidebar
-                          ? "active-width justify-content-start ms-4"
-                          : "justify-content-center"
-                      } align-items-center ${
-                        activeLink === sideItem.label
-                          ? "active-side-link"
-                          : null
-                      }`}
-                      onClick={() => {
-                        setActiveLink(sideItem.label);
-                        sideItem.label === "Swap"
-                          ? props.logout()
-                          : setActiveLink(sideItem.label);
-                      }}
-                      onMouseEnter={() => setHover(sideItem.label)}
-                      onMouseLeave={() => setHover(null)}
-                    >
-                      <img
-                        src={
-                          require(`../../assets/sidebarIcons/${
-                            activeLink === sideItem.label ||
-                            hover === sideItem.label
-                              ? sideItem.icon + "Active.svg"
-                              : sideItem.icon + ".svg"
-                          }`).default
-                        }
-                        alt=""
-                        style={{ width: 32, height: 32 }}
-                      />
-                      {activeSidebar && (
-                        <h3
-                          className={
-                            activeLink === sideItem.label ||
-                            hover === sideItem.label
-                              ? "active-text"
-                              : "sideitem-text"
-                          }
-                        >
-                          {sideItem.label}
-                        </h3>
-                      )}
-                    </div>
-                  </NavLink>
-                )
-              )
-            )}
-          </div>
-        </div>
+        <img
+          src={activeSidebar ? toolsLogoActive : toolsLogo}
+          alt=""
+          style={{ height: "40px" }}
+        /></NavLink>
       </div>
+      <div
+        className={`sidebar-container w-100 justify-content-center ${
+          activeSidebar ? "align-items-start" : "align-items-center"
+        } d-flex flex-column gap-4`}
+        style={{ gap: 35 }}
+      >
+         <div className={`sidebar-container w-100 accordion justify-content-center ${
+          activeSidebar ? "align-items-start" : "align-items-center"
+        } d-flex flex-column gap-4`} id="accordionExample2" 
+            >
+        {sidebarItems.map((sideItem, index) =>
+          sideItem.children?.length > 0 ? (
+           
+              <>
+              <div
+                data-bs-toggle="collapse"
+                data-bs-target={`#collapse${sideItem.label}`}
+                aria-expanded="true"
+                aria-controls={`collapse${sideItem.label}`}
+                id={`heading${sideItem.label}`}
+                className={`sidebar-item gap-3 p-2 d-flex ${
+                  activeSidebar
+                    ? "active-width justify-content-start ms-4"
+                    : "justify-content-center"
+                } align-items-center ${
+                  activeLink === sideItem.label ? "active-side-link" : null
+                }`}
+                onClick={() => setActiveLink(sideItem.label)}
+                onMouseEnter={() => setHover(sideItem.label)}
+                onMouseLeave={() => setHover(null)}
+              >
+                <img
+                  src={
+                    require(`../../assets/sidebarIcons/${
+                      activeLink === sideItem.label || hover === sideItem.label
+                        ? sideItem.icon + "Active.svg"
+                        : sideItem.icon + ".svg"
+                    }`).default
+                  }
+                  alt=""
+                  style={{ width: 32, height: 32 }}
+                />
+                {activeSidebar && (
+                  <div className="d-flex w-100 flex-row align-items-center justify-content-between">
+                    <h3
+                      className={
+                        activeLink === sideItem.label ||
+                        hover === sideItem.label
+                          ? "active-text"
+                          : "sideitem-text"
+                      }
+                    >
+                      {sideItem.label}
+                    </h3>
+                    <img src={accordionIndicator} alt="indicator" id="indicator"  />
+                  </div>
+                )}
+              </div>
+              <div
+                id={`collapse${sideItem.label}`}
+                className={`accordion-collapse collapse ${sideItem.children.filter(obj => {return windowUrl.includes(obj.link)}) ? 'open' : null}`}
+                aria-labelledby={`heading${sideItem.label}`}
+                data-bs-parent="#accordionExample2"
+              >
+                {activeSidebar ? (
+                  <div className="accordion-container d-flex flex-column gap-2 ms-5">
+                    {sideItem.children.map((child, index) => (
+                     <NavLink key={index} to={child.link}  className={isActive => isActive ? 'accordion-child accordion-child-active' : 'accordion-child'}>
+                      {child.title}
+                     </NavLink>
+                    ))}
+                  </div>
+                ) : null}
+              </div>
+              </>
+          ) : (
+           sideItem.link?.length > 0 &&
+           <NavLink to={sideItem.link}>
+            <div
+           key={index}
+           id={sideItem.label}
+           className={`sidebar-item gap-3 p-2 d-flex ${
+             activeSidebar
+               ? "active-width justify-content-start ms-4"
+               : "justify-content-center"
+           } align-items-center ${
+             activeLink === sideItem.label ? "active-side-link" : null
+           }`}
+           onClick={() => setActiveLink(sideItem.label)}
+           onMouseEnter={() => setHover(sideItem.label)}
+           onMouseLeave={() => setHover(null)}
+         >
+           <img
+             src={
+               require(`../../assets/sidebarIcons/${
+                 activeLink === sideItem.label || hover === sideItem.label
+                   ? sideItem.icon + "Active.svg"
+                   : sideItem.icon + ".svg"
+               }`).default
+             }
+             alt=""
+             style={{ width: 32, height: 32 }}
+           />
+           {activeSidebar && (
+             <h3
+               className={
+                 activeLink === sideItem.label || hover === sideItem.label
+                   ? "active-text"
+                   : "sideitem-text"
+               }
+             >
+               {sideItem.label}
+             </h3>
+           )}
+         </div>
+           </NavLink>
+          )
+        )}
+            </div>
+
+      </div>
+     </div>
     </div>
     // <div
     //   onClick={props.toggleMobileSidebar}
@@ -824,6 +800,7 @@ const Sidebar = (props) => {
     //     )}
     //   </div>
     // </div>
+    
   );
 };
 
